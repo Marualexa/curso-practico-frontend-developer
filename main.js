3d87b88c050f13469ef5063d2de75790712ce0d9
Mariana <contrerasomanam@gmail.com>
2022-10-25 18:45:14 -0500

    unificacion de los menus

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.deskton-menu');
+const menuEsteIcon = document.querySelector('.menu');
+const movilMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 emailMenu.addEventListener('click', toggleDesktopMenu);
+menuEsteIcon.addEventListener('click', toggleMovilMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 
+function toggleMovilMenu() {
+    movilMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    let mobilMenuOpen = movilMenu.classList.contains('inactive')
+
+    aside.classList.toggle('inactive');
+
+    if (movilMenu.classList.contains('inactive'))
+}
